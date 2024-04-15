e87212c7580111e3140bfbaef5c1457be58bfa19
yeray <yeray@Yeray>
2024-04-15 17:57:16 +0200

    commit git

--- Ejemplo1/src/paquete1/Principal.java
+++ Ejemplo1/src/paquete1/Principal.java
@@ -5,7 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones con Git");
 		System.out.println("Escribo otro mensaje");
-		
 
 	}
 
